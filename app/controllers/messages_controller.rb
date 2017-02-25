20925010c65eb5242dbd8532b3f2e6f9a315ce46
Jio Sotoyama <Jio@Jios-MacBook-Air.local>
2017-02-25 21:24:48 +0900

    メッセージ機能修正２

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,29 @@
 class MessagesController < ApplicationController
 
-  def index
-    @groups = current_user.groups
-    @message = Message.new
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
-  end
-
-  def create
-    @message = Message.create(message_params)
-    if @message.save
-    redirect_to group_messages_path
-    else
-    flash[:notice] = "本文を入力してください。"
-    redirect_to group_messages_path
+    def index
+      @groups = current_user.groups
+      @message = Message.new
+      @group = Group.find(params[:group_id])
+      @messages = @group.messages
     end
-  end
+
+    def create
+      @message = Message.create(message_params)
+      if @message.save
+        redirect_to group_messages_path
+      else
+        flash[:notice] = "本文を入力してください。"
+        redirect_to group_messages_path
+      end
+    end
 
 
 
 
-private
-  def message_params
-  params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
-  end
+  private
+    def message_params
+      params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    end
 end
 
 
